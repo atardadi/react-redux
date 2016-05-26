c7ecc4dd7d1498b016b9e5295bfa3beb5a951fa2
Dadi Atar <atardadi@gmail.com>
2016-05-26 09:53:31 +0300

    coure list row impl

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
+import {loadCourses} from './actions/courseActions';
 import './styles/styles.css';
 import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 
 const store = configureStore();
+store.dispatch(loadCourses());
 
 render(
   <Provider store={store}>
